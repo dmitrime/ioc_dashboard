f9f8956c5470e5f94f4f18bf99c0c4d42980596a
Dmitri <dmitri807@gmail.com>
2012-03-21 22:18:28 +0200

    sidebar

--- dashboard.py
+++ dashboard.py
@@ -4,15 +4,16 @@ from models import app, tg_ioc, host_binary
 
 @app.route('/_get_iocs')
 def get_iocs():
-    columns = ['id', 'title']
-    return get_datatable_items(tg_ioc, columns)
+    columns = ['title']
+    return get_datatable_items(tg_ioc, columns, tg_ioc.title)
 
 @app.route('/_get_host_binaries')
 def get_host_binaries():
-    columns = ['id', 'host_guid']
-    return get_datatable_items(host_binary, columns)
+    columns = ['host_guid']
+    return get_datatable_items(host_binary, columns, host_binary.host_guid)
 
-def get_datatable_items(table, columns):
+def get_datatable_items(table, columns, group):
+    print(type(group))
     # sorting
     order = []
     sortingCols = int(request.args.get('iSortingCols', 0))
@@ -29,13 +30,13 @@ def get_datatable_items(table, columns):
         where[columns[1]] = search
 
     # record count before filtering
-    count = table.query.count()
+    count = table.query.group_by(group).count()
 
     start = int(request.args.get('iDisplayStart', 0))
     limit = int(request.args.get('iDisplayLength', 10))
 
     # main query with filter, order and paginate
-    query = table.query.filter_by(**where)
+    query = table.query.group_by(group).filter_by(**where)
     filtered_count = query.count()
     query = query.order_by(*order).limit(limit).offset(start)
     print query
@@ -44,12 +45,29 @@ def get_datatable_items(table, columns):
     rows = []
     for r in results:
         vals = [getattr(r, sel) for sel in columns]
+        vals.append(0)
         rows.append(vals)
     d = {   'sEcho':request.args.get('sEcho',0),
             'iTotalRecords':count,
             'iTotalDisplayRecords':filtered_count,
             'aaData':rows}
     return jsonify(d)
+
+@app.route('/_populate_iocs')
+def populate_iocs():
+    results = tg_iocs.query.all()
+    rows = []
+    for r in results:
+        rows.append([r.title, r.id])
+    return jsonify(rows)
+
+@app.route('/_populate_hosts')
+def populate_hosts():
+    results = host_binary.query.distinct(host_binary.host_guid)
+    rows = []
+    for r in results:
+        rows.append([r.title, r.id])
+    return jsonify(rows)
 
 @app.route('/')
 def hello():
